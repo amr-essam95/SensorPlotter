1be9bcd9f63eced56e4b85dbcfbd9e8de170e44f
amr <amr.essam95@hotmail.com>
2020-12-08 04:51:56 +0200

    handling sending data to the server using queues

--- src/connection_utils.py
+++ src/connection_utils.py
@@ -3,6 +3,8 @@ from PyQt5.QtCore import Qt
 import socket
 import struct
 import threading
+import select
+import queue
 
 class SocketCommunicator(QtCore.QObject):
 
@@ -17,25 +19,45 @@ class SocketCommunicator(QtCore.QObject):
 		self.socketConnection = ''
 		self.socketConnectionSucceeded = False
 
+		# Any data received by this queue will be sent
+		self.send_queue = queue.Queue()
+		# Any data sent to ssock shows up on rsock
+		self.rsock, self.ssock = socket.socketpair()
+
 	def connect(self):
 		try:
 			self.socketConnectionSucceeded = True
 			self.socketConnection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			self.socketConnection.connect((self.hostname, self.port))
+
 		except:
 			self.socketConnectionSucceeded = False
 			print ("Connection with Server failed.")
 
 	def sendData(self, data):
 		if self.socketConnectionSucceeded == True:
-			self.socketConnection.sendall(data)
+			# Put the data to send inside the queue
+			self.send_queue.put(data)
+   		 	# Sending data to ssock which goes to rsock
+			self.ssock.send(b"\x00")
 		else:
 			print ("Connection is closed, please connect first.")
 
 	def receiveData(self):
 		while True:
-			data = self.socketConnection.recv(60)
-			if not data: break
-			structList = struct.unpack("iiiiiiihhHHHHHHHHBBBhhhh",data)
-			self.dataReady.emit(structList[0], structList[4], structList[5], structList[6], structList[2], structList[3], structList[9], structList[10], structList[11], structList[12], structList[13], structList[14], structList[15], structList[16])
-			self.labelDataReady.emit(structList[17], structList[19])
+			# When either socket has data or rsock has data, select.select will return
+			rlist, _, _ = select.select([self.socketConnection, self.rsock], [], [])
+			for ready_socket in rlist:
+				if ready_socket is self.socketConnection:
+					data = self.socketConnection.recv(60)
+					if not data: continue
+					structList = struct.unpack("iiiiiiihhHHHHHHHHBBBhhhh",data)
+					self.dataReady.emit(structList[0], structList[4], structList[5], structList[6], structList[2], structList[3], structList[9], structList[10], structList[11], structList[12], structList[13], structList[14], structList[15], structList[16])
+					self.labelDataReady.emit(structList[17], structList[19])
+				else:
+					# Ready_socket is rsock
+					self.rsock.recv(1)  # Dump the ready mark
+					# Send the data.
+					dataToSend = self.send_queue.get()
+					print (dataToSend)
+					self.socketConnection.sendall(dataToSend)
